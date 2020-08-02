425cfbda5d1a48f644df7b3da9063825ce8748c8
Nexus <tyfeng98@163.com>
2020-08-02 23:04:09 +0800

    update the WBMatrix in WBAES test

--- test/WBAES/WBMatrix/test/main.c
+++ test/WBAES/WBMatrix/test/main.c
@@ -64,41 +64,38 @@ void asciiStr2hex (char * in, char * out, int len){
 
 int main(int argc, char * argv[])
 {
-        //unsigned char OUT[16];
-        //unsigned char OOUT[16];
-        //unsigned char IN[32];
-        int i;
-        u8 expandedKey[176];
-        u8 key[16] = {0};
-        uint64_t begin;
-        uint64_t end;
-        uint64_t ans = 0;
-        
-        //printf("Generating tables...\n");
-        expandKey (key, expandedKey);
+    //unsigned char OUT[16];
+    //unsigned char OOUT[16];
+    //unsigned char IN[32];
+    int i;
+    u8 expandedKey[176];
+    u8 key[16] = {0};
+    uint64_t begin;
+    uint64_t end;
+    uint64_t ans = 0;
+    
+    expandKey (key, expandedKey);
 
-        printf("WBAES-WBMatrix performance test:\n");
-        begin = start_rdtsc();
-        for (i = 0; i < TEST; i++)
-        {
-                computeTables(expandedKey);
-        }
-        end = end_rdtsc();
-        ans = (end - begin);
-        printf("WBAES generate tables cost %llu CPU cycles\n", ans/TEST);
+    printf("WBAES-WBMatrix performance test:\n");
+    begin = start_rdtsc();
+    for (i = 0; i < TEST; i++)
+    {
+        computeTables(expandedKey);
+    }
+    end = end_rdtsc();
+    ans = (end - begin);
+    printf("WBAES generate tables cost %llu CPU cycles\n", ans/TEST);
+    
+    //unsigned char IN[16] = {0x00,0x11,0x22,0x33,0x44,0x55,0x66,0x77,0x88,0x99,0xaa,0xbb,0xcc,0xdd,0xee,0xff};
+    //unsigned char IN[16] = {0x0,0x1,0x2,0x3,0x4,0x5,0x6,0x7,0x8,0x9,0xa,0xb,0xc,0xd,0xe,0xf};
+    //printstate(IN);
+    //aes_128_table_encrypt(IN, OUT);
+    //printstate(OUT);
+    
+    //unsigned char IIN[16] = {0x00,0x11,0x22,0x33,0x44,0x55,0x66,0x77,0x88,0x99,0xaa,0xbb,0xcc,0xdd,0xee,0xff};
+    //unsigned char IIN[16] = {0x0,0x1,0x2,0x3,0x4,0x5,0x6,0x7,0x8,0x9,0xa,0xb,0xc,0xd,0xe,0xf};
+    //aes_128_encrypt(IIN, OOUT);
+    //printstate(OOUT);
 
-        //asciiStr2hex(argv[1], (char *)IN, 32);
-        
-        //unsigned char IN[16] = {0x00,0x11,0x22,0x33,0x44,0x55,0x66,0x77,0x88,0x99,0xaa,0xbb,0xcc,0xdd,0xee,0xff};
-        //unsigned char IN[16] = {0x0,0x1,0x2,0x3,0x4,0x5,0x6,0x7,0x8,0x9,0xa,0xb,0xc,0xd,0xe,0xf};
-        //printstate(IN);
-        //aes_128_table_encrypt(IN, OUT);
-        //printstate(OUT);
-        
-        //unsigned char IIN[16] = {0x00,0x11,0x22,0x33,0x44,0x55,0x66,0x77,0x88,0x99,0xaa,0xbb,0xcc,0xdd,0xee,0xff};
-        //unsigned char IIN[16] = {0x0,0x1,0x2,0x3,0x4,0x5,0x6,0x7,0x8,0x9,0xa,0xb,0xc,0xd,0xe,0xf};
-        //aes_128_encrypt(IIN, OOUT);
-        //printstate(OOUT);
-
-        return 0;
+    return 0;
 }
